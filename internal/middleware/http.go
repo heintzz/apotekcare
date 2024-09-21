4c0c927d1e50d04598019b67697ed8305459989f
heintzz <mhasnanr@gmail.com>
2024-09-21 21:54:46 +0700

    feat: add logger and token middleware

---
+++ internal/middleware/http.go
@@ -0,0 +1,70 @@
+package middleware
+
+import (
+	"context"
+	"heintzz/ecommerce/internal/helper"
+	"heintzz/ecommerce/internal/utils"
+	"log"
+	"time"
+
+	"net/http"
+	"strings"
+)
+
+func Tracer(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		now := time.Now()
+    
+		log.Printf("method=%v path=%v type=[INFO] message='incoming request'", r.Method, r.URL.Path)
+    
+		h.ServeHTTP(w, r)
+    
+		end := time.Since(now).Milliseconds()
+    
+		log.Printf("method=%v path=%v type=[INFO] message='finish request' response_time=%vms", r.Method, r.URL.Path, end)
+	})
+}
+
+func CheckToken(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		bearer := r.Header.Get("Authorization")
+
+		if bearer == "" {
+			resp := helper.APIResponse{
+				HttpCode:  http.StatusUnauthorized,				
+				Message:   "no token provided",
+			}
+			resp.WriteJsonResponse(w)
+			return
+		}
+
+		tokenSlice := strings.Split(bearer, "Bearer ")
+
+		if len(tokenSlice) < 2 {
+			resp := helper.APIResponse{
+				HttpCode:  http.StatusUnauthorized,
+				Error: 		 "unauthorized",
+				Message:   "invalid token",
+			}
+			resp.WriteJsonResponse(w)
+			return
+		}
+
+		tokenString := tokenSlice[1]
+		token, err := utils.VerifyToken(tokenString)
+		
+		if err != nil {			
+			resp := helper.APIResponse{
+				HttpCode:  http.StatusUnauthorized,
+				Error: 		 "unauthorized",				
+			}
+			resp.WriteJsonResponse(w)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "AUTH_EMAIL", token.Email)		
+		r = r.WithContext(ctx)
+
+		h.ServeHTTP(w, r)
+	})
+}
